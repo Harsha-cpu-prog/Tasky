4abf25e3fffc13dadfa03e3ad811137953a9e82f
Harsha-cpu-prog <harshanenwani121@gmail.com>
2021-09-14 15:34:36 +0530

    add tasky project

---
+++ index.js
@@ -0,0 +1,9 @@
+const saveChanges=()=>{
+    const taskData={
+        id:`${Date.now()}`,
+        imageUrl:document.getElementById("imageurl"),
+        taskTitle:document.getElementById("imageurl"),
+        taskType:document.getElementById("imageurl"),
+        taskDescription:document.getElementById("imageurl"),
+    }
+}
